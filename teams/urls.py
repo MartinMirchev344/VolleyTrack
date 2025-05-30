5a3eac39424c06c3e5a57eddf89f88d279762a52
MartinMirchev344 <martin.m.mirchev.2023@elsys-bg.org>
2025-05-30 10:51:43 +0300

    updated routes for player and matches

--- teams/urls.py
+++ teams/urls.py
@@ -1,7 +1,9 @@
 from rest_framework.routers import DefaultRouter
-from .views import TeamViewSet
+from .views import TeamViewSet, PlayerViewSet, MatchViewSet
 
 router = DefaultRouter()
 router.register(r'teams', TeamViewSet)
+router.register(r'players', PlayerViewSet)
+router.register(r'matches', MatchViewSet)
 
 urlpatterns = router.urls
