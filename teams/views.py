68a29924932d5157f935722efa784ad9dee71e32
MartinMirchev344 <martin.m.mirchev.2023@elsys-bg.org>
2025-05-30 10:46:26 +0300

    added viewset for player

--- teams/views.py
+++ teams/views.py
@@ -2,7 +2,13 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Team
 from .serializers import TeamSerializer
+from .models import Player
+from .serializers import PlayerSerializer
 
 class TeamViewSet(viewsets.ModelViewSet):
     queryset = Team.objects.all()
     serializer_class = TeamSerializer
+
+class PlayerViewSet(viewsets.ModelViewSet):
+    queryset = Player.objects.all()
+    serializer_class = PlayerSerializer
